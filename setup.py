764fc88f0cfa13fc253ef3e4039b36d975b018a5
Cody D Dixon <130433943+CodeMasterCody3D@users.noreply.github.com>
2025-04-20 09:36:36 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ plugin_package = "octoprint_bambu_printer"
 plugin_name = "OctoPrint-BambuPrinter"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.1.7"
+plugin_version = "0.1.8rc15"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
@@ -33,14 +33,17 @@ plugin_url = "https://github.com/jneilliii/OctoPrint-BambuPrinter"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = ["paho-mqtt<2", "python-dateutil", "pybambu>=1.0.1"]
+plugin_requires = ["paho-mqtt<2", "python-dateutil", "cloudscraper"]
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
 ### --------------------------------------------------------------------------------------------------------------------
 
-# Additional package data to install for this plugin. Since you have only a Python file, no templates or static files are needed
-plugin_additional_data = []
+# Additional package data to install for this plugin. The subfolders "templates", "static" and "translations" will
+# already be installed automatically if they exist. Note that if you add something here you'll also need to update
+# MANIFEST.in to match to ensure that python setup.py sdist produces a source distribution that contains all your
+# files. This is sadly due to how python's setup.py works, see also http://stackoverflow.com/a/14159430/2028598
+plugin_additional_data = ["octoprint_bambu_printer/printer/pybambu/filaments.json"]
 
 # Any additional python packages you need to install with your plugin that are not contained in <plugin_package>.*
 plugin_additional_packages = []
@@ -49,8 +52,8 @@ plugin_additional_packages = []
 plugin_ignored_packages = []
 
 # Additional parameters for the call to setuptools.setup. If your plugin wants to register additional entry points,
-# define dependency links or other things like that, this is the place to go. Will be merged recursively with
-# the default setup parameters as provided by octoprint_setuptools.create_plugin_setup_parameters using
+# define dependency links or other things like that, this is the place to go. Will be merged recursively with the
+# default setup parameters as provided by octoprint_setuptools.create_plugin_setup_parameters using
 # octoprint.util.dict_merge.
 #
 # Example:
@@ -58,14 +61,7 @@ plugin_ignored_packages = []
 #     additional_setup_parameters = {"dependency_links": ["https://github.com/someUser/someRepo/archive/master.zip#egg=someDependency-dev"]}
 # "python_requires": ">=3,<4" blocks installation on Python 2 systems, to prevent confused users and provide a helpful error.
 # Remove it if you would like to support Python 2 as well as 3 (not recommended).
-additional_setup_parameters = {
-    "python_requires": ">=3.9,<4",
-    "entry_points": {
-        "octoprint.plugin": [
-            "bambu_printer = octoprint_bambu_printer.bambu_printer:BambuPrinterPlugin",  # Entry point for the plugin class
-        ],
-    }
-}
+additional_setup_parameters = {"python_requires": ">=3.9,<4"}
 
 ########################################################################################################################
 
@@ -73,12 +69,13 @@ from setuptools import setup
 
 try:
     import octoprint_setuptools
-except ImportError:
+except:
     print(
         "Could not import OctoPrint's setuptools, are you sure you are running that under "
         "the same python installation that OctoPrint is installed under?"
     )
     import sys
+
     sys.exit(-1)
 
 setup_parameters = octoprint_setuptools.create_plugin_setup_parameters(
@@ -99,6 +96,7 @@ setup_parameters = octoprint_setuptools.create_plugin_setup_parameters(
 
 if len(additional_setup_parameters):
     from octoprint.util import dict_merge
+
     setup_parameters = dict_merge(setup_parameters, additional_setup_parameters)
 
 setup(**setup_parameters)
