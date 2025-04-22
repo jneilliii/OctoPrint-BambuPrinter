195eb03594a29d78ba4869331a8194d189f2f1b9
Cody D Dixon <130433943+CodeMasterCody3D@users.noreply.github.com>
2025-04-22 06:59:29 -0500

    Update auto-bambu-cam.sh

--- BambuONEclickADDon&CAM/auto-bambu-cam-and-watchdog.sh
+++ BambuONEclickADDon&CAM/auto-bambu-cam-and-watchdog.sh
@@ -2,4 +2,4 @@
 
 source "$HOME/bambucam/bin/activate"
 python "$HOME/OctoPrint-BambuPrinter/BambuONEclickADDon&CAM/bambucam.py" &
-python "$HOME/OctoPrint-BambuPrinter/BambuONEclickADDon&CAM/working_watchdog.py" &
+
